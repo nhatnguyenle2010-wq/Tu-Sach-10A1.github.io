5af9203c3fa30c635d069ff074d264c0fbe5782b
Nhat <nhatnguyenle.2010@gmail.com>
2026-02-24 11:59:20 +0700

    DE

--- Phim/phim-ai.js
+++ Phim/phim-ai.js
@@ -85,7 +85,7 @@ function initGrass() {
 const CARDS = [
   {
     label: 'Video 1', emoji: '🦆',
-    src:   'https://nhatnguyenle2010-wq.github.io/Tu-Sach-10A1.github.io/Phim/vid/video-1.mp4',
+    src:   'https://drive.google.com/file/d/1FvP_IzZ1GDZzwk0-bYSzSgqgsJOVxVN9/view?usp=drive_link',
     title: 'Đất Rừng Phương Nam',
   },
   {
